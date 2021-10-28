f2d35d7df6295e658167d480ee852c7a3adfa30f
lexa <62031288+akg453@users.noreply.github.com>
2021-10-28 17:33:29 -0400

    Fixed player wall issues

--- Assets/scripts/walls.cs
+++ Assets/scripts/walls.cs
@@ -6,25 +6,19 @@ public class walls : MonoBehaviour
 {
 
     float playerWalk = 0;
-
-    public class PlayerWalk : MonoBehaviour
-    {
-        public float speed = 3.0f;
-    }
+    public PlayerWalk pWalk;
 
     // Start is called before the first frame update
     public void Start()
     {
-        GameObject player = GameObject.Find("player");
-        PlayerWalk playerWalk = player.GetComponent<PlayerWalk>();
-        playerWalk.speed = 3.0f;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "player")
         {
-            playerWalk.speed = 0.0f;
+            //pWalk.speed = 0.0f;
         }
     }
 
